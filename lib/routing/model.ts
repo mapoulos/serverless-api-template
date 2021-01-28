721f1c4780220e8a68b355d479c7a49feb30cf34
Alex Poulos <alex.poulos@washpost.com>
2021-01-28 16:08:53 -0500

    prettier

--- lib/routing/model.ts
+++ lib/routing/model.ts
@@ -1,29 +1,39 @@
-export interface Route  {
-    httpMethod: HTTPMethod
-    pathPattern: string
-    handler: RouteHandler
+export interface Route {
+  httpMethod: HTTPMethod;
+  pathPattern: string;
+  handler: RouteHandler;
 }
 
 export interface RouteHandlerInput {
-    pathParameters: Record<string,string>
-    queryStringParameters: Record<string,string>
-    body: Object | string
-    headers?: Record<string,string>
+  pathParameters: Record<string, string>;
+  queryStringParameters: Record<string, string>;
+  body: Object | string;
+  headers?: Record<string, string>;
 }
 
-export type RouteHandler = (params: RouteHandlerInput) => Promise<RouteHandlerResponse>
+export type RouteHandler = (
+  params: RouteHandlerInput
+) => Promise<RouteHandlerResponse>;
 
 export interface RouteHandlerResponse {
-    statusCode: number,
-    body: Object | string,
-    headers?: Record<string,string> 
+  statusCode: number;
+  body: Object | string;
+  headers?: Record<string, string>;
 }
-
 
 export interface LambdaResponse {
-    statusCode: number,
-    body: string,
-    headers?: Record<string,string>
+  statusCode: number;
+  body: string;
+  headers?: Record<string, string>;
 }
 
-export type HTTPMethod = 'GET' | 'HEAD' | 'POST' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE' | 'PATCH' 
+export type HTTPMethod =
+  | "GET"
+  | "HEAD"
+  | "POST"
+  | "PUT"
+  | "DELETE"
+  | "CONNECT"
+  | "OPTIONS"
+  | "TRACE"
+  | "PATCH";
